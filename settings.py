49567f24bd478e5e550af23ccc3078c9de934d8f
Christian Carl <missingunknown78@gmail.com>
2026-01-19 10:26:21 +0800

    New menu style for mobile view

--- settings.py
+++ settings.py
@@ -4,16 +4,16 @@ import os
 ON_PYTHONANYWHERE = 'PYTHONANYWHERE_DOMAIN' in os.environ
 
 if ON_PYTHONANYWHERE:
-    # PythonAnywhere MySQL credentials (replace with your actual username, password, database)
-    MYSQL_USER = "evotingprototype"                               # your PythonAnywhere MySQL username
-    MYSQL_PASSWORD = "student_votingdatabase"                     # your PythonAnywhere MySQL password
-    MYSQL_HOST = "evotingprototype.mysql.pythonanywhere-services.com"  # host
-    MYSQL_DB = "evotingprototype$student_voting"                  # database name
-    SECRET_KEY = os.environ.get("SECRET_KEY", "sabanal")          # you can leave this using env
+    
+    MYSQL_USER = "evotingprototype"                               #PythonAnywhere MySQL username
+    MYSQL_PASSWORD = "student_votingdatabase"                     #PythonAnywhere MySQL password
+    MYSQL_HOST = "evotingprototype.mysql.pythonanywhere-services.com"  #host
+    MYSQL_DB = "evotingprototype$student_voting"                  #database name
+    SECRET_KEY = os.environ.get("SECRET_KEY", "sabanal")          #move to venv
 else:
     # Local XAMPP MySQL credentials
     MYSQL_USER = "root"
-    MYSQL_PASSWORD = ""                                           # usually empty for XAMPP
+    MYSQL_PASSWORD = ""                                          
     MYSQL_HOST = "localhost"
     MYSQL_DB = "student_voting"
     SECRET_KEY = "sabanal"
