76e8bde284a8a81d8b33f6b4475f56fac8547b35
Christian Carl <missingunknown78@gmail.com>
2026-04-02 10:34:31 +0800

    Use SSL port 465 for faster email

--- settings.py
+++ settings.py
@@ -60,9 +60,9 @@ if not SECRET_KEY:
 
 # ============= EMAIL CONFIGURATION =============
 MAIL_SERVER = os.environ.get('MAIL_SERVER', '142.250.150.108')
-MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
-MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'True').lower() == 'true'
-MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'False').lower() == 'true'
+MAIL_PORT = int(os.environ.get('MAIL_PORT', 465))
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'False').lower() == 'true'
+MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'True').lower() == 'true'
 MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
